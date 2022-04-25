a1fc6c2fd1a5de44657315dbe6e80b5348165111
mtaech <mtaech@outlook.com>
2022-04-25 15:06:46 +0800

    add global db connect

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,10 +1,32 @@
 #![cfg_attr(
-  all(not(debug_assertions), target_os = "windows"),
-  windows_subsystem = "windows"
+    all(not(debug_assertions), target_os = "windows"),
+    windows_subsystem = "windows"
 )]
 
+use dotenv::dotenv;
+use once_cell::sync::OnceCell;
+use sea_orm::DatabaseConnection;
+use std::env;
+use tauri::async_runtime::block_on;
+
+mod init;
+
+static DB_POOL: OnceCell<DatabaseConnection> = OnceCell::new();
+
+pub struct GlobalEnv {}
+
+impl GlobalEnv {
+    pub fn global_db_pool() -> &'static DatabaseConnection {
+        DB_POOL.get().expect("get sqlite connect instance error")
+    }
+}
+
 fn main() {
-  tauri::Builder::default()
-    .run(tauri::generate_context!())
-    .expect("error while running tauri application");
+    init::setup_logger();
+    dotenv().ok();
+    let connection: DatabaseConnection = block_on(init::set_db_pool());
+    DB_POOL.set(connection);
+    tauri::Builder::default()
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
 }
